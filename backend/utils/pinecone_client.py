faf11400f40fd37a1e61087fd9dcce70de894f8c
Emre Can Baykurt <ebaykurt@bu.edu>
2025-07-13 14:11:49 -0400

    Update pinecone_client.py

--- backend/utils/pinecone_client.py
+++ backend/utils/pinecone_client.py
@@ -1,14 +1,13 @@
-# backend/utils/pinecone_client.py
-
-import pinecone
 import os
+import pinecone
 
+# Init Pinecone
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-PINECONE_ENV = os.getenv("PINECONE_ENVIRONMENT")
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
 PINECONE_INDEX = os.getenv("PINECONE_INDEX")
 
-pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
 index = pinecone.Index(PINECONE_INDEX)
 
-def query_vector(query_embedding: list, top_k=3):
-    return index.query(vector=query_embedding, top_k=top_k, include_metadata=True)
+def query_vector(embedding: list, top_k: int = 3):
+    return index.query(vector=embedding, top_k=top_k, include_metadata=True)
